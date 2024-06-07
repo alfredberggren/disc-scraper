a5eb22806b497a399b28f3655d4e117eabbe8d12
Alfred Berggren <alfred.berggren@gmail.com>
2024-06-07 18:38:31 +0200

    format

--- scraper/disc.py
+++ scraper/disc.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Disc:
     """
     Class representing a disc found in document by parser.
     """
+
     mold_name: str
     plastic: str
     manufacturer: str
     price: int
-
